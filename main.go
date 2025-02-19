4dcdf7401fd64be8a8f8e6d25ac4c343314ab7a8
guilherme-souza-lima <guilherme_souza.lima@hotmail.com>
2025-02-19 16:17:15 -0300

    deploy

--- main.go
+++ main.go
@@ -11,12 +11,21 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-var webSocketByUserMutex sync.Mutex
 var wsUpgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
 }
+
+var (
+	webSocketByUserMutex sync.Mutex
+	connections          = make(map[*websocket.Conn]bool)
+	wsUpgrade            = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
 
 func main() {
 	e := echo.New()
@@ -25,6 +34,7 @@ func main() {
 
 	e.GET("/check", healthCheck)
 	e.GET("/ws", wsFunc)
+	e.GET("/ws2", wsFunc2)
 
 	certFile := "fullchain.pem"
 	keyFile := "privkey.pem"
@@ -71,4 +81,65 @@ func wsFunc(c echo.Context) error {
 	}
 
 	return nil
+}
+
+func wsFunc2(c echo.Context) error {
+	ws, err := wsUpgrade.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		log.Println("Erro ao atualizar para WebSocket:", err)
+		return err
+	}
+	defer ws.Close()
+
+	webSocketByUserMutex.Lock()
+	connections[ws] = true
+	webSocketByUserMutex.Unlock()
+
+	log.Println("Novo cliente conectado!")
+
+	// Ouve por mensagens do cliente e faz o broadcast para todos os conectados
+	for {
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			log.Println("Erro ao ler mensagem:", err)
+			break
+		}
+
+		// Imprime a mensagem recebida
+		log.Printf("Mensagem recebida: %s\n", msg)
+
+		// Envia a mensagem para todos os WebSockets conectados
+		broadcastMessage(msg)
+
+		// Envia uma resposta de confirmação ao cliente
+		err = ws.WriteMessage(websocket.TextMessage, []byte("Mensagem recebida"))
+		if err != nil {
+			log.Println("Erro ao enviar mensagem:", err)
+			break
+		}
+	}
+
+	// Remove a conexão WebSocket da lista quando o cliente se desconectar
+	webSocketByUserMutex.Lock()
+	delete(connections, ws)
+	webSocketByUserMutex.Unlock()
+
+	log.Println("Cliente desconectado!")
+
+	return nil
+}
+
+// Função para fazer o broadcast da mensagem para todos os WebSockets conectados
+func broadcastMessage(msg []byte) {
+	// Envia a mensagem para todos os WebSockets conectados
+	webSocketByUserMutex.Lock()
+	for conn := range connections {
+		err := conn.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			log.Println("Erro ao enviar mensagem para WebSocket:", err)
+			conn.Close()
+			delete(connections, conn)
+		}
+	}
+	webSocketByUserMutex.Unlock()
 }
